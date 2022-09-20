d6975391465e8d663e145ac0b5f9a8cb66682312
Zennitt <112974436+Zennitt@users.noreply.github.com>
2022-09-20 16:08:53 -0300

    Mais cods:
    using TicTacToe;
    
    
    new JogoDaVelha().Iniciar();
    
    bool reiniciarJogo = true;
    
    Console.WriteLine("Deseja refazer? <S> <N>");
    string resp = Console.ReadLine();
    if (resp == "S")
    {
        Console.Clear();
        new JogoDaVelha().Iniciar();
    }
    else
    {
        reiniciarJogo = false;
    
    }

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TicTacToe;
+
+
+new JogoDaVelha().Iniciar();
+
+bool reiniciarJogo = true;
+
+Console.WriteLine("Deseja refazer? <S> <N>");
+string resp = Console.ReadLine();
+if (resp == "S")
+{
+    Console.Clear();
+    new JogoDaVelha().Iniciar();
+}
+else
+{
+    reiniciarJogo = false;
+
+}
+
+
